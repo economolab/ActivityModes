50aede79c56df5f31dc7e4e4bbb829a131b06ef9
jackieb1 <54858424+jackieb1@users.noreply.github.com>
2022-01-19 16:13:07 -0500

    Update plotAllModes.m

--- plotting/plotAllModes.m
+++ plotting/plotAllModes.m
@@ -19,24 +19,27 @@ for i = 1:numel(fns)
         latent = mySmooth(psth(:,:,cond)*rez.(fns{i}),plt.smooth);
         plot(rez.time,latent,'Color',plt.colors{j},'LineWidth',plt.lw(j));
     end
-    xline(sample,'k--','LineWidth',0.5);
-    xline(delay,'k--','LineWidth',0.5);
+%     xline(sample,'k--','LineWidth',0.5);
+%     xline(delay,'k--','LineWidth',0.5);
     xline(zeroEv,'k--','LineWidth',0.5);
-    title(fns{i}(1:end-5))
+    title(fns{i}(1:end-5),'FontSize',13)
     xlim([-2.5,2.1])
+    ax=gca;
+    ax.FontSize=11;
     hold off
 end
 
 leg = legend(plt.legend);
 leg.Position = [0.60,0.066,0.28,0.13];
+leg.FontSize = 14;
 sgtitle(plt.title)
 
-% h=axes(fig,'visible','off');
-% h.XLabel.Visible='on';
-% h.YLabel.Visible='on';
-% ylabel(h,'Activity (a.u.)');
-% 
-% xlabel(h,['Time (s) from ' alignEv]);
+h=axes(fig,'visible','off');
+h.XLabel.Visible='on';
+h.YLabel.Visible='on';
+ylabel(h,'Activity (a.u.)','FontSize',14);
+
+xlabel(h,['Time (s) from ' alignEv],'FontSize',14);
 
 end % plotAllModes
 
