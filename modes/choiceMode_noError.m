9c565b40ba994793ba89b7c6d7f94a8837e49c0f
jackieb1 <54858424+jackieb1@users.noreply.github.com>
2022-02-18 11:21:47 -0500

    New choiceMode function
    Choice Mode function for calculating choice mode without including error trials in the definition

---
+++ modes/choiceMode_noError.m
@@ -0,0 +1,29 @@
+function choicemode = choiceMode_noError(obj,meta,cond,epoch,alignEvent)
+% 2. choice mode: defined during delay period
+%       (hitR - hitL) / sqrt(sum(sd for each tt ^2));
+
+% which trials to use for each condition used for finding the mode
+trials = getTrialsForModeID(obj,cond);
+
+if isfield(obj,'earlyMoveix')
+    trials.ix(obj.earlyMoveix,:) = 0;
+elseif isfield(obj,'earlyMoveTrial')
+    trials.ix(obj.earlyMoveTrial,:) = 0;
+end
+
+% find time in each trial corresponding to epoch
+epochix = nan(obj.bp.Ntrials,2);
+for trix = 1:obj.bp.Ntrials
+    epochix(trix,:) = findedges_FirstLick(obj.time,obj.bp,epoch,trix,alignEvent); % (idx1,idx2)
+end
+
+epochMean = getEpochMean(obj,epochix,trials,meta);
+
+[mu,sd] = getEpochStats(epochMean,meta,trials);
+
+% calculate mode according to definition
+choicemode = (mu(:,1)-mu(:,2))./ sqrt(sum(sd.^2,2));
+choicemode(isnan(choicemode)) = 0;
+choicemode = choicemode./sum(abs(choicemode)); % (ncells,1)
+
+end % choiceMode
